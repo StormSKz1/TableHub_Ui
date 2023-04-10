8efcb14f0125e007218c65126096c88981ffbce5
StormSKz1 <83227365+StormSKz1@users.noreply.github.com>
2023-04-10 07:34:43 +0700

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'Shikku Hub'
+	SaveManager.Folder = 'Table Hub'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
